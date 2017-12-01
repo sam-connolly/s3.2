49590c0870323a8ddddf81ef96641f0f00c5a8b4
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2017-12-01 12:46:10 +0000

    PWM for Red and Green shield LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
-#include <mbed.h>
+#include "mbed.h"
+#include "C12832.h"
 
-int main() {
-    //PwmOut led(LED3);
-    DigitalOut r(LED1);
+// Using Arduino pin notation
+C12832 lcd(D11, D13, D12, D7, D10);
 
-    r.write(0);
-    //led.period(2.0f);
-    //led.write(0.5f);
-    while(1) ;
+
+PwmOut   sred(D5);
+PwmOut sgreen(D9);
+//PwmOut  sblue(D8); /* not connected to PWM output from K64F */
+
+
+int main()
+{
+    int j=0;
+    lcd.cls();
+    lcd.locate(0,3);
+    lcd.printf("mbed application shield!");
+
+    while(true) {   // this is the third thread
+        lcd.locate(0,15);
+        lcd.printf("Counting : %4d ",j++);
+        wait(1.0);
+    }
 }
