0253af9774d9795cae3e643f7bbab3a79bbe3676
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2017-12-05 14:12:49 +0000

    library use

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,21 @@
 #include "mbed.h"
-#include "C12832.h"
 
-// Using Arduino pin notation
-C12832 lcd(D11, D13, D12, D7, D10);
-
-
-PwmOut   sred(D5);
+//PwmOut   sred(D5);
 PwmOut sgreen(D9);
-//PwmOut  sblue(D8); /* not connected to PWM output from K64F */
-
 
 int main()
 {
-    int j=0;
-    lcd.cls();
-    lcd.locate(0,3);
-    lcd.printf("mbed application shield!");
-
-    while(true) {   // this is the third thread
-        lcd.locate(0,15);
-        lcd.printf("Counting : %4d ",j++);
-        wait(1.0);
+  /*
+    float f;
+    while(1) {
+        for (f = 1.00; f > 0; f-=0.01 ) {
+            sred.write(f);
+            printf(" %f \n", f);
+            wait(0.5);
+        }
     }
+  */
+  sgreen.period(4.0f);
+  sgreen.write(0.25f);
+  while(1);
 }
