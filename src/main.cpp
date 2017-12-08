f4140b40bfed55c3c47b8a842384fc961176b072
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2017-12-08 15:03:21 +0000

    sine wave pwm

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,26 @@
 #include "mbed.h"
 
-//PwmOut   sred(D5);
+PwmOut sred(D5);
 PwmOut sgreen(D9);
 
+const float Freq = 4000;
+const int DTms = 20;
 int main()
 {
-  /*
+  int t = 0;
+  float p,q;
+  sgreen.period_ms(10);
+  sred.period_ms(10);
+
+  while(1){
     float f;
-    while(1) {
-        for (f = 1.00; f > 0; f-=0.01 ) {
-            sred.write(f);
-            printf(" %f \n", f);
-            wait(0.5);
-        }
-    }
-  */
-  sgreen.period(4.0f);
-  sgreen.write(0.25f);
-  while(1);
+    t += DTms;
+    f = t/Freq *2.f * M_PI;
+    p = (1+sin(f))/2;
+    q = (1+sin(f+M_PI))/2;
+    sgreen.write(p);
+    sred.write(q);
+    wait_ms(DTms);
+  }
+
 }
