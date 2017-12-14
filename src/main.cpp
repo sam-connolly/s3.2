fb3df5fd12fc29998ad9bf9c6e73925f3de1e897
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2017-12-14 16:00:05 +0000

    simple ramp

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,22 @@
 #include "mbed.h"
 
 PwmOut sred(D5);
-PwmOut sgreen(D9);
 
-const float Freq = 4000;
-const int DTms = 20;
 int main()
 {
-  int t = 0;
-  float p,q;
-  sgreen.period_ms(10);
-  sred.period_ms(10);
+DigitalOut green(D6);
+green.write(1);
+
+  sred.period_ms(10);/* suitably fast 100Hz */
 
   while(1){
-    float f;
-    t += DTms;
-    f = t/Freq *2.f * M_PI;
-    p = (1+sin(f))/2;
-    q = (1+sin(f+M_PI))/2;
-    sgreen.write(p);
-    sred.write(q);
-    wait_ms(DTms);
+      /* ramp up from 0 to 100% */
+      float duty;
+      for( duty=0 ; duty<1.0 ; duty+=0.01 ) {
+          sred.write(duty);
+          wait_ms(100);
+      }
+
   }
 
 }
