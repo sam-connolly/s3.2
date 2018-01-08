e621c803feaa01f401ad16d7562be1d73e7f9206
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2018-01-08 07:54:19 +0000

    Changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,17 @@
 #include "mbed.h"
 
-PwmOut sred(D5);
+PwmOut red(D5);
 
 int main()
 {
-DigitalOut green(D6);
-green.write(1);
+    red.period_ms(10);/* suitably fast 100Hz */
 
-  sred.period_ms(10);/* suitably fast 100Hz */
-
-  while(1){
-      /* ramp up from 0 to 100% */
-      float duty;
-      for( duty=0 ; duty<1.0 ; duty+=0.01 ) {
-          sred.write(duty);
-          wait_ms(100);
-      }
-
-  }
-
+    while(1){
+        /* ramp up from 0 to 100% */
+        float duty;
+        for( duty=0 ; duty<1.0 ; duty+=0.01 ) {
+            red.write(duty);
+            wait_ms(100);
+        }
+    }
 }
