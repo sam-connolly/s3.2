fefd5f7a3c2d36a5e573ea5aac4e49845d50c2da
sam-connolly <sam.connolly@northumbria.ac.uk>
2018-02-01 10:46:44 +0000

    Brightness controlled by right potentiometer

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,19 @@ int main()
 {
     red.period_ms(10);/* suitably fast 100Hz */
 
+    AnalogIn right(A1);
+    float rightVolt;
+
     while(1){
+        rightVolt = 3.3*(right.read());
+
         /* ramp up from 0 to 100% */
         float duty;
-        for( duty=0 ; duty<1.0 ; duty+=0.01 ) {
-            red.write(duty);
+        for ( duty=0 ; duty<4.6 ; duty+=0.23 ) {
+            float brightness = exp(duty)/100;
+            red.write(right.read());
             wait_ms(100);
+            printf("The brightness is %f\n", brightness);
         }
     }
 }
